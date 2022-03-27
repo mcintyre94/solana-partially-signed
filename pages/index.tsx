9e72f4773862adb6ad74ef0f15149c0b30b9103d
Callum McIntyre <callum@pointer.gg>
2022-03-27 11:43:05 +0100

    Add wallet connect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { useWallet } from '@solana/wallet-adapter-react'
+
 
 export default function IndexPage() {
   const [loading, setLoading] = useState(false)
+  const { publicKey, sendTransaction } = useWallet()
 
   async function handleClick() {
     setLoading(true)
-    const res = await fetch('/api/makeTransaction', { method: 'post' })
+    const res = await fetch(
+      '/api/makeTransaction',
+      {
+        method: 'post',
+        body: JSON.stringify({ account: publicKey.toBase58() }),
+      })
     const data = await res.json()
     console.log(data.transaction)
     setLoading(false)
@@ -13,6 +22,8 @@ export default function IndexPage() {
 
   return (
     <>
+      <WalletMultiButton />
+      <br />
       <button type="button" onClick={handleClick}>Create Transaction</button>
       {loading && <p>Loading...</p>}
     </>
